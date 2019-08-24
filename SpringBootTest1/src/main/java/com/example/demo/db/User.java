6f5eff2902cbf355febab604d1f5e80040620203
invikari <2855801715@qq.com>
2019-08-24 15:13:15 +0800

    test for spring boot

---
+++ SpringBootTest1/src/main/java/com/example/demo/db/User.java
@@ -0,0 +1,49 @@
+package com.example.demo.db;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+    @Id
+    private Integer id;
+    private String username;
+    private String password;
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
