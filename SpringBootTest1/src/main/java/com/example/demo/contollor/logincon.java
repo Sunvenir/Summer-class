b9528228baeaf6ff267594ee662d05c191f09505
Souvenir <604610263@qq.com>
2019-08-26 13:30:19 +0800

    删除多余

--- SpringBootTest1/src/main/java/com/example/demo/contollor/logincon.java
+++ SpringBootTest1/src/main/java/com/example/demo/contollor/logincon.java
@@ -5,10 +5,7 @@ import com.example.demo.Repository.DaoImgl;
 import com.example.demo.domain.User;
 import com.example.demo.result.Result;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
@@ -16,6 +13,7 @@ import java.util.Map;
 
 @Controller
 public class logincon {
+    @CrossOrigin
     @RequestMapping(value = "/login")
     public Result login( @RequestBody User requestUser){
         Dao dao = DaoImgl.getInstance();
@@ -28,6 +26,7 @@ public class logincon {
         }
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/user/register")
     public Result register(@RequestBody User requestUser){
         Dao dao = DaoImgl.getInstance();
